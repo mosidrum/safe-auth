58e554d3e0b58fcaa585db7e25c29e4cf5446727
mosidrum <mosiokanga@gmail.com>
2024-11-30 06:53:14 +0100

    add winston logger

---
+++ backend/src/services/logger.ts
@@ -0,0 +1,29 @@
+import {createLogger, format, transports} from 'winston'
+import {TransformableInfo} from "logform";
+
+const { combine, timestamp, printf } = format;
+
+const defaultFormatter = printf(
+    ({ level, message, timestamp }: TransformableInfo) =>
+        `${timestamp} [${level}] : ${message}`,
+);
+
+const commonFormat = combine(
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    defaultFormatter,
+);
+
+export const logger = createLogger({
+    level: process.env.LOG_LEVEL || "info",
+    format: commonFormat,
+    transports: [
+        new transports.Console(),
+        new transports.File({
+            filename: "logs/error.log",
+            level: "error",
+        }),
+        new transports.File({
+            filename: "logs/combined.log",
+        }),
+    ],
+});
